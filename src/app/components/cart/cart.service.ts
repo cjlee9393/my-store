781abc4f670cf1533133a2c7338b9d095d5a8125
cjlee <cjlee9393@gmail.com>
2022-09-27 09:39:49 +0900

    write list of service and its function

--- src/app/components/cart/cart.service.ts
+++ src/app/components/cart/cart.service.ts
@@ -8,11 +8,35 @@ export class CartService {
 
   constructor() { }
 
+  getCart(): Map<number, number> {
+    return this.cart;
+  }
+
   addToCart(productId: number, quantity: number): void {
     if (this.cart.has(productId)){
       this.cart.set(productId, this.cart.get(productId) as number + quantity);
     }else{
       this.cart.set(productId, quantity)
     }
+  }
+
+  changeQuantity(productId: number, quantity: number): void {
+    if (!this.cart.has(productId)){
+      const err = new Error();
+      err.name = "productNotInCartError";
+      err.message = "product does not exist in cart";
+      throw err;
+    }
+
+    if (quantity == 0){
+      alert('removed from cart!');
+      this.cart.delete(productId);
+    }else{
+      this.cart.set(productId, quantity);
+    }
+  }
+
+  reset(): void {
+    this.cart = new Map()
   }
 }
